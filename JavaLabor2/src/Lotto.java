58ec67949762a00467185635bb44b96694ef119f
PIPAPUTZEMANN <sebas@DESKTOP-9GCLEFI.fritz.box>
2016-11-12 18:59:47 +0100

    To do: auf dopellte Zahlen prüfen

--- JavaLabor2/src/Lotto.java
+++ JavaLabor2/src/Lotto.java
@@ -4,11 +4,19 @@ public class Lotto {
     
 	public static void main(String[] args) {
 	
-	int[] test=drawLottery();
+	int[] AusgabeZiehung=drawLottery();
 	
-	for(int q=0;q<test.length;q++){
-	System.out.println(test[q]);
+	for(int q=0;q<AusgabeZiehung.length;q++){
+	System.out.println(AusgabeZiehung[q]);
 	}
+	
+	
+	int[] EingabeZahlen=readPlayerTip();{
+		
+		
+	}
+	
+	
 		
 		
 	}
@@ -23,7 +31,7 @@ public class Lotto {
 	
 }
 	
-	public static int [] drawLottery(){
+	public static int[] drawLottery(){
 		
 		int i;
 		int[] feld=new int [6];
@@ -40,5 +48,36 @@ public class Lotto {
 		
 	}
 
+	public static int[] readPlayerTip(){
 		
+	int r=0;
+	int zaehler;
+	int y=0;
+	
+	int[] eingabeZahl= new int[6];
+	
+	for(zaehler=0;zaehler<6;zaehler++){
+		
+		r++;
+	
+	System.out.println("Bitte geben sie die "+r+" . ein!");
+		y=In.readInt();
+		
+		if (y<=0 || y>49){
+		    System.out.println("Zahl nicht gültig");
+		    System.exit( 0 );}
+		
+
+	
+	else {
+		eingabeZahl[zaehler]=y;
+	}
+		
+	
+	}
+		return eingabeZahl;
+		
+	}
+	
+	
 }
