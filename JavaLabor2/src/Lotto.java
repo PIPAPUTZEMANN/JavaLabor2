fa21c13f3fc95b176ec421bed3d47fb4f5dedc1b
PIPAPUTZEMANN <sebas@DESKTOP-9GCLEFI.fritz.box>
2016-11-12 15:12:08 +0100

    Lotto Zuffallszahlen funktionieren jedoch noch doppelte möglich

--- JavaLabor2/src/Lotto.java
+++ JavaLabor2/src/Lotto.java
@@ -1,8 +1,44 @@
-
 public class Lotto {
-
+	
+	
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	int[] test=drawLottery();
+	
+	for(int q=0;q<test.length;q++){
+	System.out.println(test[q]);
+	}
+		
+		
+	}
+	
+	public static int  drawLotteryNumber(){
+	
+	int z;
+	
+	z=(int) (Math.random() * 49 + 1); 
+	
+	return z;
+	
+}
+	
+	public static int [] drawLottery(){
+		
+		int i;
+		int[] feld=new int [6];
+		
+		for(i=0;i<feld.length;i++){
+			
+			
+			feld[i]=drawLotteryNumber();
+			
+			
+			
+		}
+		return feld;
+		
 	}
 
+		
 }
